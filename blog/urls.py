3859c30aa95b2fb9efb065f8d0cfdb08c32f1458
Nurbosh <omurov.nurbol25@gmail.com>
2021-02-01 22:12:30 +0600

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
